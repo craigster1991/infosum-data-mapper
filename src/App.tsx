0cb99b3182c514df749759a52300b221a76c32a5
Craig Wheatley <craigster1991@gmail.com>
2024-11-22 17:02:30 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss'
+import { Heading } from './components/Heading/Heading'
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <Heading/>
+    </div>
+  )
+}
+
+export default App
