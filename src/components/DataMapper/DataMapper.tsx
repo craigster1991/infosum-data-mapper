941c1f8ecb2db8e6cc9a9cd7f9086d2f51099857
Craig Wheatley <craigster1991@gmail.com>
2024-11-24 21:46:36 +0000

    build out rows and cols for data mapper

--- src/components/DataMapper/DataMapper.tsx
+++ src/components/DataMapper/DataMapper.tsx
@@ -5,7 +5,7 @@ import DataContext from '../../contexts/dataContext'
 export const DataMapper = () => {
 
   const { customerData: customerData, customerDataKeys: customerDataKeys } = useContext(DataContext)
-  const [ page, setPage ] = useState(1)
+  const [page, setPage] = useState(1)
 
   const handlePageChange = (page: number) => {
     // can't go below 1 or above the number of pages
@@ -13,25 +13,41 @@ export const DataMapper = () => {
   }
 
   return (
-    <div className={styles.columns}>
-      <button onClick={() => handlePageChange(page - 1)}>previous page</button>
-      <button onClick={() => handlePageChange(page + 1)}>next page</button>
-      <div className={styles.column}>
-        <h2>Your data</h2>
-        {
-          // if we have data and keys, map and render
-          customerData[page] && customerDataKeys && customerDataKeys.map((key: string) => (
-            <div key={key}>
-              <span>{key}</span>
-              <span>{customerData[page][key]}</span>
-            </div>
-          ))
-        }
+    <>
+      {/* TODO // CREATE NAV COMPONENT */}
+      <div className={styles.nav}>
+        <input type="text" placeholder="search" />
+        <button onClick={() => handlePageChange(page - 1)}>previous page</button>
+        <button onClick={() => handlePageChange(page + 1)}>next page</button>
       </div>
-      <div className={styles.column}>
-        <h2>Mapped fields</h2>
 
+      <div className={styles.columns}>
+          <div className={styles.row}>
+            <h2 className={styles.col}>Your Data</h2>
+            <div className={`${styles.col} ${styles.arrowCol}`}></div>
+            <h2 className={styles.col}>Mapped Fields</h2>
+          </div>
+          {
+            // if we have data and keys, map and render
+            customerData[page] && customerDataKeys && customerDataKeys.map((key: string) => (
+              <div className={styles.row} key={key}>
+                <div className={styles.col}>
+                  {/* TODO // CREATE CARD COMPONENT */}
+                  <div className={styles.card} key={key}>
+                    <span>{key}</span>
+                    <span>{customerData[page][key] || "\u00A0"}</span>
+                  </div>
+                </div>
+                <div className={`${styles.col} ${styles.arrowCol}`}>{'➡️'}</div>
+                <div className={styles.col}>
+                  {/* TODO // USE CARD COMPONENT */}
+                  merge
+                </div>
+              </div>
+            ))
+          }
       </div>
-    </div>
+
+    </>
   )
 }
