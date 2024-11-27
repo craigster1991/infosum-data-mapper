ac07ab3bb26c2e75816a86ac36529655f7734fae
Craig Wheatley <craigster1991@gmail.com>
2024-11-27 00:52:06 +0000

    creates Hint component

---
+++ src/components/Hint/Hint.tsx
@@ -0,0 +1,16 @@
+import { CONSTANTS } from '../../utils/constants'
+import styles from './Hint.module.scss'
+
+interface HintProps {
+  type: "auto" | "manual",
+  className?: string
+}
+
+export const Hint = ({ type, className }: HintProps) => {
+  return (
+    <div className={`${styles.hint} ${className} ${CONSTANTS.CONVERT_TYPES.AUTO && styles.auto}`}>
+      <p>{type === CONSTANTS.CONVERT_TYPES.AUTO ? "automagic!" : "manual."}</p>
+    </div>
+  )
+}
+
