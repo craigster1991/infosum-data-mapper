139270d79108f3fdcde00faaffa65cae1165a048
Craig Wheatley <craigster1991@gmail.com>
2024-11-27 16:10:43 +0000

    Updates

--- src/utils/types.ts
+++
@@ -1,8 +0,0 @@
-export type mappingDataType = {
-  [key: string]: {
-    from: string,
-    to: string,
-    type: "auto" | "manual"
-  }
-}
-export type handleMapChangeType = (e: any, inputKey: string) => void
